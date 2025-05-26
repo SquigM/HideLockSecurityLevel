826c063daf3fd8ccc8cede6d5c6417ac6eca2a6f
SquigM <106306784+SquigM@users.noreply.github.com>
2025-05-26 10:48:51 +0100

    Added configurable lock level text via INI file

---
+++ src/config.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <REX/REX.h>
+
+namespace HideLockSecurityLevel
+{
+	class Config
+	{
+		REX::INI::Str<REX::INI::SettingStore> lock_text;
+
+		Config() :
+			lock_text("Config", "lock_text", "Locked")
+		{
+			auto store = REX::INI::SettingStore::GetSingleton();
+			store->Init(INI_PATH, "");
+			store->Load();
+			store->Save();
+		}
+
+	public:
+		static constexpr const char* INI_PATH = "OblivionRemastered/Binaries/Win64/OBSE/Plugins/HideLockSecurityLevel.ini";
+		static Config*               GetSingleton()
+		{
+			static Config config = Config();
+			return &config;
+		}
+
+		std::string LockText() { return lock_text.GetValue(); }
+	};
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,23 @@
+#include "config.h"
+
 namespace Hook
 {
 	struct Hook_SPrintF
 	{
 		static std::int64_t BSStringSPrintFFunc(RE::BSString& outName, const char* fmtString, const char* arg)
 		{
-			return BSStringSPrintFFuncHook(outName, fmtString, "Locked");
+			return BSStringSPrintFFuncHook(outName, fmtString, HideLockSecurityLevel::Config::GetSingleton()->LockText().c_str());
 		}
 
 		static inline REL::Hook BSStringSPrintFFuncHook{ REL::Offset(0x6912370), 0xEB6, BSStringSPrintFFunc };
-	};	
+	};
 }
 
 OBSE_PLUGIN_LOAD(const OBSE::LoadInterface* a_obse)
 {
 	OBSE::Init(a_obse, { .trampoline = true, .trampolineSize = 32 });
+
+	HideLockSecurityLevel::Config::GetSingleton();
 
 	return true;
 }
